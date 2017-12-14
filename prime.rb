c6694393e92d243a8616968f5b12b8545e4dac80
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-14 18:28:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,5 @@
 def prime?(num)
   return false if num<=1
-  Math.sqrt(num).to_i.downto(2).each do |i|
-    return false if num % i == 0
-    true
-  end
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
 end
