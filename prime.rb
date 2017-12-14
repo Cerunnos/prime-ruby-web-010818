e12c67d3527fe8be142d018ce59f38ba4a2379ce
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-14 18:23:25 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(num)
   return false if num<=1
   prime_numbers=[]
   square_root=Math.sqrt(num)
-  instance=(2..square_root)
+  instance=(2..square_root).to_a
   instance.each do|i|
     if num % i == 0
       return false
