86306434a91d5cc978666af7f67fbb508ebe6355
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-14 18:09:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,9 @@ def prime?(int)
   instance=(2..int).to_a
   prime_numbers=[]
   instance.each do|i|
-    if (int % i) == 0 && i == int
+    if (int % i) == 0 && i != int
+    elsif (int % i) == && i == int
+      prime_numbers<<int
     end
   end
   prime_numbers.include?(int)
